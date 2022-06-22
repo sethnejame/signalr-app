34ca5f1e268aa0a79640fdd28c4a57665b8ddd8a
Seth <seth@nejame.dev>
2022-06-22 15:51:28 +0200

    Adding counter implementation

---
+++ SignalRApp/Hubs/CounterHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApp.Hubs
+{
+    public class CounterHub : Hub
+    {
+        public Task AddToTotal(string user, int value)
+        {
+            return Clients.All.SendAsync("CounterIncremented", user, value);
+        }
+    }
+}
--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -11,10 +11,16 @@ namespace WPFClient;
 public partial class MainWindow : Window
 {
     HubConnection connection;
+    HubConnection counterConnection;
 
     public MainWindow()
     {
         InitializeComponent();
+
+        counterConnection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7287/counterhub")
+            .WithAutomaticReconnect()
+            .Build();
 
         connection = new HubConnectionBuilder()
             .WithUrl("https://localhost:7287/chathub")
@@ -57,7 +63,41 @@ public partial class MainWindow : Window
             return Task.CompletedTask;
         };
 
+        counterConnection.Reconnecting += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Attempting to reconnect counter connection. . .";
+                messages.Items.Add(newMessage);
+            });
 
+            return Task.CompletedTask;
+        };
+
+        counterConnection.Reconnected += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Reconnected to the counter server!";
+                messages.Items.Clear();
+                messages.Items.Add(newMessage);
+            });
+
+            return Task.CompletedTask;
+        };
+
+        counterConnection.Closed += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Counter connection closed.";
+                messages.Items.Add(newMessage);
+                openConnection.IsEnabled = true;
+                sendMessage.IsEnabled = false;
+            });
+
+            return Task.CompletedTask;
+        };
     }
 
     private async void openConnection_Click(object sender, RoutedEventArgs e)
@@ -90,6 +130,30 @@ public partial class MainWindow : Window
         {
             await connection.InvokeAsync("SendMessage",
                 "WPF Client", messageInput.Text);
+        }
+        catch (Exception ex)
+        {
+            messages.Items.Add(ex.Message);
+        }
+    }
+
+    private async void openCounterConnection_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await counterConnection.StartAsync();
+        }
+        catch (Exception ex)
+        {
+            messages.Items.Add(ex.Message);
+        }
+    }
+
+    private async void incrementCounter_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await counterConnection.InvokeAsync("AddToTotal", "WPF Cliet", 1);
         }
         catch (Exception ex)
         {
